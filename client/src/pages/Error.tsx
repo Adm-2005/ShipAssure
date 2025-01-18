5196712c4bf41fd623e82a663f128f8aeae68d0b
Your Name <you@example.com>
2025-01-19 00:26:58 +0530

    added src files

---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Error = () => {
+  return (
+    <div className="flex flex-col min-h-screen justify-between">
+      <Navbar />
+
+      {/* error */}
+      <div></div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Error;
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col min-h-screen justify-between">
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default SignUp;
